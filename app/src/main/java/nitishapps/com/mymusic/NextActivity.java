4ebdf6a2c7a80cc3cb150a75e2b2934008ad800a
Nitish Agarwal <luv.nits@gmail.com>
2017-07-27 20:25:21 +0530

    Feature : Music Control Buttons added

--- app/src/main/java/nitishapps/com/mymusic/NextActivity.java
+++ app/src/main/java/nitishapps/com/mymusic/NextActivity.java
@@ -5,17 +5,33 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class NextActivity extends AppCompatActivity {
     ArrayList songPath;
-    MediaPlayer mp;
+    Intent i;
+    Button b_play, b_pause, b_stop;
+    TextView tv_cd;
+    TextView tv_td;
+    static SeekBar sb;
+    static MediaPlayer mp;
+    //static int position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+
+
+
+
+
+
 
 
         //position from Main Activity
@@ -24,8 +40,9 @@ public class NextActivity extends AppCompatActivity {
         int index = i.getIntExtra("in",-1);
         songPath = i.getIntegerArrayListExtra("songPath");
         Toast.makeText(this,songPath.get(index).toString(), Toast.LENGTH_SHORT).show();
-        mp= MediaPlayer.create(this, Uri.parse(songPath.get(index).toString()));
-        mp.start();
+
+        //mp= MediaPlayer.create(this, Uri.parse(songPath.get(index).toString()));
+        //mp.start();
 
 
     }
